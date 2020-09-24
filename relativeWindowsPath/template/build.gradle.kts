ae98a862e6369d349df2814322533aaab999c391
Martin Bonnin <martin@mbonnin.net>
2020-09-24 14:48:11 +0200

    added an exemple with a relative path to try to make it fail on windows

---
+++ relativeWindowsPath/template/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+  kotlin("jvm").version("1.4.0")
+  id("com.apollographql.apollo").version("2.4.0")
+}
+
+repositories {
+  mavenCentral()
+  mavenLocal()
+}
+dependencies {
+  implementation(kotlin("stdlib"))
+  testImplementation(kotlin("test-junit"))
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+apollo {
+  service("service") {
+    sourceFolder.set("com/library")
+    schemaPath.set("../../../schema.sdl")
+  }
+}
